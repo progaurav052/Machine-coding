245c24189e957e40f69294dfed4dfba9ca99161f
gaurav pai <gpprojects511@gmail.com>
2025-07-17 09:53:45 +0530

    Initialized Repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-blue-200 m-2 p-4 rounded-lg shadow-lg">
+      <h1 className="font-bold text-xl ">Machine Coding </h1>
     </div>
   );
 }
